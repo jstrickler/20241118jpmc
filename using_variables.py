350b2a0423477b68c49ca2a2d982acfc8e78b8fa
John Strickler <jstrickler@gmail.com>
2024-11-18 17:37:09 +0000

    initial commit

---
+++ using_variables.py
@@ -0,0 +1,15 @@
+x = 5
+print(x)
+print(x * 10)
+
+#  a-z 0-9 _ A-Z
+
+# _spam  "private"
+# __foo__   "dunder" -- predefined (aka MAGIC)
+
+m = None  # null
+
+class_break = "12 noon"
+
+# dir = "north"
+
